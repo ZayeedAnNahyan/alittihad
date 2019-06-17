7959e9cd653d0865882ecf5908600fa63ba182ca
Priyambodo <maspriyambodo@gmail.com>
2019-06-17 14:43:18 +0700

    update welcome.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -6,8 +6,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
 
-	<style type="text/css">
-
 	::selection { background-color: #E13300; color: white; }
 	::-moz-selection { background-color: #E13300; color: white; }
 
